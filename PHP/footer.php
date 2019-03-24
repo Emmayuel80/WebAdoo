4e4544c518bf17a7afccc6d5a6ed81e878596919
Silvestre <emmayuel80@gmail.com>
2019-03-24 14:54:16 -0600

    Añadido codigo php para el header y el footer

---
+++ PHP/footer.php
@@ -0,0 +1,14 @@
+<?php
+echo '<footer class="mdl-mini-footer">
+<div class="mdl-mini-footer__left-section">
+    <div class="mdl-logo">Sistema Gestor de Libreria</div>
+    <ul class="mdl-mini-footer__link-list">
+        <li><a href="#">Help</a></li>
+        <li><a href="#">Privacy & Terms</a></li>
+    </ul>
+</div>
+</footer>
+</div>
+</main>
+</div>';
+?>
---
+++ PHP/header.php
@@ -0,0 +1,30 @@
+<?php
+echo '    <!-- Always shows a header, even in smaller screens. -->
+<div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+    <header class="mdl-layout__header">
+        <div class="mdl-layout__header-row">
+            <!-- Title -->
+            <span class="mdl-layout-title">Sistema Gestor de Libreria</span>
+            <!-- Add spacer, to align navigation to the right -->
+            <div class="mdl-layout-spacer"></div>
+            <!-- Navigation. We hide it in small screens. -->
+            <nav class="mdl-navigation mdl-layout--large-screen-only">
+                <a class="mdl-navigation__link" href="index.html">Inicio</a>
+                <a class="mdl-navigation__link" href="requerimientos.html">Requerimientos</a>
+                <a class="mdl-navigation__link" href="casosu.html">Casos de Uso</a>
+                <a class="mdl-navigation__link" href="">Link</a>
+            </nav>
+        </div>
+    </header>
+    <div class="mdl-layout__drawer">
+        <span class="mdl-layout-title">SGL</span>
+        <nav class="mdl-navigation">
+            <a class="mdl-navigation__link" href="index.html">Inicio</a>
+            <a class="mdl-navigation__link" href="requerimientos.html">Requerimientos</a>
+            <a class="mdl-navigation__link" href="casosu.html">Casos de Uso</a>
+            <a class="mdl-navigation__link" href="">Link</a>
+        </nav>
+    </div>
+    <main class="mdl-layout__content">
+        <div class="page-content">';
+?>
